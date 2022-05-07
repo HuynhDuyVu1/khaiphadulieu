080edba49d47b4cb6bfaf775038896130b4c46f1
HuynhDuyVu1 <vuhuynh459748@gmail.com>
2022-05-07 17:37:08 +0700

    first commit

--- WekaPro/src/wekapro/WekaPro.java
+++ WekaPro/src/wekapro/WekaPro.java
@@ -16,11 +16,17 @@ public class WekaPro {
      */
     public static void main(String[] args) throws Exception {
         // TODO code application logic here
-        MyKnowledgeModel model = new MyKnowledgeModel(
-                "C:\\Program Files\\Weka-3-8-6\\data\\iris.arff");
+//        MyKnowledgeModel model = new MyKnowledgeModel(
+//                "C:\\Program Files\\Weka-3-8-6\\data\\iris.arff");
+//        System.out.println(model);
+//        model.saveData("D:\\Data\\iris.arff");
+//        model.saveData2CSV("D:\\Data\\iris.csv");
+        MyAprioriModel model = new MyAprioriModel(
+                "C:\\Program Files\\Weka-3-8-6\\data\\weather.numeric.arff",
+                "-N 10 -T 0 -C 0.9 -D 0.05 -U 1.0 -M 0.1 -S -1.0 -c -1",
+                "-R 2-3" );
+        model.mineAssociationRules();
         System.out.println(model);
-        model.saveData("D:\\Data\\iris.arff");
-        model.saveData2CSV("D:\\Data\\iris.csv");
     }
     
 }
